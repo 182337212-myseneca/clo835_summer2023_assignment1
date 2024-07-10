b0a3e4815a47b9bf3b8a193b036f5317fe5eab3e
182337212-myseneca <153153154+182337212-myseneca@users.noreply.github.com>
2024-07-10 09:48:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,34 +7,22 @@ from urllib.parse import quote as url_quote
 
 app = Flask(__name__)
 
-# Set environment variables
-os.environ["DBHOST"] = "localhost"
-os.environ["DBUSER"] = "root"
-os.environ["DBPWD"] = "password"
-os.environ["DATABASE"] = "employees"
-os.environ["DBPORT"] = "3306"
-os.environ["APP_COLOR"] = "lime"
-
-DBHOST = os.environ.get("DBHOST")
-DBUSER = os.environ.get("DBUSER")
-DBPWD = os.environ.get("DBPWD")
-DATABASE = os.environ.get("DATABASE")
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+DBHOST = os.environ.get("DBHOST") or "localhost"
+DBUSER = os.environ.get("DBUSER") or "root"
+DBPWD = os.environ.get("DBPWD") or "passwors"
+DATABASE = os.environ.get("DATABASE") or "employees"
+COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
 DBPORT = int(os.environ.get("DBPORT"))
 
 # Create a connection to the MySQL database
-try:
-    db_conn = connections.Connection(
-        host=DBHOST,
-        port=DBPORT,
-        user=DBUSER,
-        password=DBPWD, 
-        db=DATABASE
-    )
-except pymysql.err.OperationalError as e:
-    print(f"Error connecting to MySQL: {e}")
-    exit(1)
-
+db_conn = connections.Connection(
+    host= DBHOST,
+    port=DBPORT,
+    user= DBUSER,
+    password= DBPWD, 
+    db= DATABASE
+    
+)
 output = {}
 table = 'employee';
 
@@ -49,11 +37,13 @@ color_codes = {
     "lime": "#C1FF9C",
 }
 
+
 # Create a string of supported colors
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
 # Generate a random color
 COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
